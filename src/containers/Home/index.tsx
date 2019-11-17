3be7727afd816daecbe1ece56729befacd1b77b7
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-17 11:19:01 +0300

    Added template for the home page

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { AppLayout } from '../../components';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>
+        Home
+    </div>
+    </AppLayout>
+  )
+};
+
+export default Home;
