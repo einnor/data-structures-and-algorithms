731660c45a23d82c7b2aac60d1213789e74d97c0
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-21 16:41:16 +0300

    DLL should add last'

--- src/lib/LinkedList/DoublyLinkedList.spec.ts
+++ src/lib/LinkedList/DoublyLinkedList.spec.ts
@@ -24,8 +24,26 @@ describe('Doubly Linked List', () => {
     expect(linkedList.head).toBe(node2);
     expect(linkedList.tail).toBe(node1);
     expect(node2.next).toBe(node1);
-    expect(node1.next).toBeNull();
     expect(node1.previous).toBe(node2);
+    expect(linkedList.length).toBe(2);
+  });
+
+  it('should add last', () => {
+    const linkedList: ILinkedList <number> = new DoublyLinkedList();
+    const node1: INode<number> = new Node({ value: 1, next: null, previous: null });
+
+    linkedList.addLast(node1);
+    expect(linkedList.head).toBe(node1);
+    expect(linkedList.tail).toBe(node1);
+    expect(linkedList.length).toBe(1);
+
+    const node2: INode<number> = new Node({ value: 2, next: null, previous: null });
+    linkedList.addLast(node2);
+    expect(linkedList.head).toBe(node1);
+    expect(linkedList.tail).toBe(node2);
+    expect(node1.next).toBe(node2);
+    expect(node2.previous).toBe(node1);
+    expect(node1.previous).toBeNull();
     expect(linkedList.length).toBe(2);
   });
 });
