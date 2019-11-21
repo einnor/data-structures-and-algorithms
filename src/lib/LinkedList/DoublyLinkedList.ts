d8453f29f3c04c460dad2a976d6fadbdb4ccfba7
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-21 16:08:43 +0300

    Doubly linked list add first

--- src/lib/LinkedList/DoublyLinkedList.ts
+++ src/lib/LinkedList/DoublyLinkedList.ts
@@ -5,6 +5,21 @@ class DoublyLinkedList <T> {
   head: INode <T> | null = null;
   tail: INode <T> | null = null;
   length: number = 0;
+
+  addFirst (node: INode <T>) : void {
+    if (this.length === 0 || this.head === null) {
+      this.head = node;
+      this.tail = node;
+      this.length = 1;
+      return;
+    }
+
+    const currentFirst = this.head;
+    node.next = currentFirst;
+    currentFirst.previous = node;
+    this.head = node;
+    this.length = this.length + 1;
+  }
 }
 
 export default DoublyLinkedList;
