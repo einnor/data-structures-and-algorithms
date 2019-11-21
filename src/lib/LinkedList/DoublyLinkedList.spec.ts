2966bfbfb0c950d0525aec92282b838a6829cb6d
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-21 16:38:29 +0300

    DLL should add first

--- src/lib/LinkedList/DoublyLinkedList.spec.ts
+++ src/lib/LinkedList/DoublyLinkedList.spec.ts
@@ -9,4 +9,23 @@ describe('Doubly Linked List', () => {
     expect(linkedList.tail).toBeNull();
     expect(linkedList.length).toBe(0);
   });
+
+  it('should add first', () => {
+    const linkedList: ILinkedList <number> = new DoublyLinkedList();
+    const node1: INode<number> = new Node({ value: 1, next: null, previous: null });
+
+    linkedList.addFirst(node1);
+    expect(linkedList.head).toBe(node1);
+    expect(linkedList.tail).toBe(node1);
+    expect(linkedList.length).toBe(1);
+
+    const node2: INode<number> = new Node({ value: 2, next: null, previous: null });
+    linkedList.addFirst(node2);
+    expect(linkedList.head).toBe(node2);
+    expect(linkedList.tail).toBe(node1);
+    expect(node2.next).toBe(node1);
+    expect(node1.next).toBeNull();
+    expect(node1.previous).toBe(node2);
+    expect(linkedList.length).toBe(2);
+  });
 });
