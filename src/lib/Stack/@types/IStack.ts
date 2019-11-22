326535bad39654b35defd99efd6c02eadf1a4586
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-22 19:01:17 +0300

    Typed stack

---
+++ src/lib/Stack/@types/IStack.ts
@@ -0,0 +1,12 @@
+import IItem from './IItem';
+import { IError } from '../../../@types';
+
+type IStack <T> = {
+  length: number;
+  push: (item: IItem <T>) => void | IError;
+  peek: () => IItem<T>;
+  pop: () => IItem<T> | IError;
+  clear: () => void;
+};
+
+export default IStack;
