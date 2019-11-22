809fbb5b4da4ce0f1c8dbaffae686cc5ef9a6b4a
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-22 20:21:31 +0300

    Stack should clear

--- src/lib/Stack/Stack.spec.ts
+++ src/lib/Stack/Stack.spec.ts
@@ -49,8 +49,11 @@ describe('Stack', () => {
     stack.push(item3);
 
     expect(stack.pop()).toEqual(item3);
+    expect(stack.length).toBe(2);
     expect(stack.pop()).toEqual(item2);
+    expect(stack.length).toBe(1);
     expect(stack.pop()).toEqual(item1);
+    expect(stack.length).toBe(0);
     expect(stack.pop()).toBeUndefined();
   });
 
@@ -66,5 +69,21 @@ describe('Stack', () => {
 
     expect(stack.enumerable().length).toBe(3);
     expect(stack.enumerable()).toEqual([item1, item2, item3]);
+  });
+
+  it('should clear', () => {
+    const stack: IStack <number> = new Stack();
+    const item1: IItem <number> = new Item({ value: 1 });
+    const item2: IItem <number> = new Item({ value: 2 });
+    const item3: IItem <number> = new Item({ value: 3 });
+
+    stack.push(item1);
+    stack.push(item2);
+    stack.push(item3);
+
+    stack.clear();
+
+    expect(stack.length).toBe(0);
+    expect(stack.enumerable()).toEqual([]);
   });
 });
