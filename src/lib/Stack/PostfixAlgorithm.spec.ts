d24d804a1a8563951760c886cab7cb58e61ec1b1
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-26 12:53:49 +0300

    PA should returns undefined if fed operators only

--- src/lib/Stack/PostfixAlgorithm.spec.ts
+++ src/lib/Stack/PostfixAlgorithm.spec.ts
@@ -1,12 +1,16 @@
 import postfixAlgorithm from './PostfixAlgorithm';
 
 describe('Postfix Algorithm', () => {
-  it('returns error if fed invalid input', () => {
+  it('should returns error if fed invalid input', () => {
     expect(postfixAlgorithm('aaa')).toEqual({ message: 'Invalid input' });
     expect(postfixAlgorithm('')).toEqual({ message: 'Invalid input' });
   });
 
-  it('returns undefined if fed numbers only', () => {
+  it('shouldreturns undefined if fed numbers only', () => {
     expect(postfixAlgorithm('123')).toBeUndefined();
+  });
+
+  it('should returns undefined if fed operators only', () => {
+    expect(postfixAlgorithm('+/-')).toBeUndefined();
   });
 });
