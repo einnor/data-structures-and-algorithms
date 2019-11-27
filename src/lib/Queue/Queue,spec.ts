d6fb71f3f7a9440cd99810fe5d7ab4e01522cd5b
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-27 18:09:57 +0300

    Queue should enqueue

--- src/lib/Queue/Queue,spec.ts
+++ src/lib/Queue/Queue,spec.ts
@@ -9,4 +9,18 @@ describe('Queue', () => {
     expect(queue.length).toBe(0);
     expect(queue.enumerable()).toEqual([]);
   });
+
+  it('should enqueue', () => {
+    const queue: IQueue <number> = new Queue();
+    const item1: IItem <number> = new Item({ value: 1 });
+
+    queue.enqueue(item1);
+    expect(queue.length).toBe(1);
+    expect(queue.enumerable()[0]).toEqual(item1);
+
+    const item2: IItem <number> = new Item({ value: 2 });
+    queue.enqueue(item2);
+    expect(queue.length).toBe(2);
+    expect(queue.enumerable()[1]).toEqual(item2);
+  });
 });
