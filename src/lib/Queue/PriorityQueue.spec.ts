ceab79aec97c2872e2fd41330da7ad9e8aed11ae
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-27 22:48:38 +0300

    PriorityQueue should enqueue

--- src/lib/Queue/PriorityQueue.spec.ts
+++ src/lib/Queue/PriorityQueue.spec.ts
@@ -10,4 +10,18 @@ describe('Priority Queue', () => {
     expect(queue.length).toBe(0);
     expect(queue.enumerable()).toEqual([]);
   });
+
+  it('should enqueue', () => {
+    const queue: IQueue <number> = new PriorityQueue();
+    const item1: IItem <number> = new Item({ value: 1, priority: 1 });
+
+    queue.enqueue(item1);
+    expect(queue.length).toBe(1);
+    expect(queue.enumerable()[0]).toEqual(item1);
+
+    const item2: IItem <number> = new Item({ value: 2, priority: 2 });
+    queue.enqueue(item2);
+    expect(queue.length).toBe(2);
+    expect(queue.enumerable()[0]).toEqual(item2);
+  });
 });
