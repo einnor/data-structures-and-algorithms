c42dd095d176f4e764f489622737041d96f9ec07
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-27 18:13:04 +0300

    Queue should peek

--- src/lib/Queue/Queue,spec.ts
+++ src/lib/Queue/Queue,spec.ts
@@ -23,4 +23,18 @@ describe('Queue', () => {
     expect(queue.length).toBe(2);
     expect(queue.enumerable()[1]).toEqual(item2);
   });
+
+  it('should peek', () => {
+    const queue: IQueue <number> = new Queue();
+
+    expect(queue.peek()).toBeUndefined();
+
+    const item1: IItem <number> = new Item({ value: 1 });
+    queue.enqueue(item1);
+    expect(queue.peek()).toEqual(item1);
+
+    const item2: IItem <number> = new Item({ value: 2 });
+    queue.enqueue(item2);
+    expect(queue.peek()).toEqual(item2);
+  });
 });
