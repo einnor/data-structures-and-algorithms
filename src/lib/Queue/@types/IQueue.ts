b52b132e010b97db481710d9358175336198af2d
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-27 17:09:45 +0300

    enqueue method

--- src/lib/Queue/@types/IQueue.ts
+++ src/lib/Queue/@types/IQueue.ts
@@ -2,9 +2,6 @@ import IItem from './IItem';
 import { IError } from '../../../@types';
 
 type IQueue <T> = {
-  length: number;
-  head: number;
-  tail: number;
   enqueue: (item: IItem <T>) => void;
   peek: () => IItem <T> | undefined;
   dequeue: () => IItem <T> | IError;
--- src/lib/Queue/Queue.ts
+++ src/lib/Queue/Queue.ts
@@ -2,14 +2,11 @@ import { IItem } from './@types';
 import { IError } from '../../@types';
 
 class Queue <T> {
-  _length: number = 0;
-
   _store: IItem <T> [] = [];
 
-  _head: number = -1;
-
-  _tail: number = -1;
-
+  enqueue (item: IItem <T>) : void {
+    this._store.push(item);
+  }
 }
 
 export default Queue;
