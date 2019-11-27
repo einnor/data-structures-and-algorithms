b9387fb8d94326b790b6fb41f4cc7dd9b35b6cb3
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-27 18:16:30 +0300

    Queue should dequeue

--- src/lib/Queue/Queue,spec.ts
+++ src/lib/Queue/Queue,spec.ts
@@ -37,4 +37,23 @@ describe('Queue', () => {
     queue.enqueue(item2);
     expect(queue.peek()).toEqual(item2);
   });
+
+  it('should dequeue', () => {
+    const queue: IQueue <number> = new Queue();
+    const item1: IItem <number> = new Item({ value: 1 });
+    const item2: IItem <number> = new Item({ value: 2 });
+    const item3: IItem <number> = new Item({ value: 3 });
+
+    queue.enqueue(item1);
+    queue.enqueue(item2);
+    queue.enqueue(item3);
+
+    expect(queue.dequeue()).toEqual(item3);
+    expect(queue.length).toBe(2);
+    expect(queue.dequeue()).toEqual(item2);
+    expect(queue.length).toBe(1);
+    expect(queue.dequeue()).toEqual(item1);
+    expect(queue.length).toBe(0);
+    expect(queue.dequeue()).toBeUndefined();
+  });
 });
