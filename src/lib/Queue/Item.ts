aa3addacbe28fefbdbf171e1314ec58e13afe0b8
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-27 16:34:28 +0300

    Queue item class

---
+++ src/lib/Queue/Item.ts
@@ -0,0 +1,11 @@
+import { IItem } from './@types';
+
+class Item <T> {
+  value: T | null;
+  priority: number | undefined;
+
+  constructor ({ value, priority }: IItem <T>) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
