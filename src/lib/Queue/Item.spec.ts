9736cc00342d6daaca03cc74fc5f61b9355c779f
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-11-28 10:19:27 +0300

    PriorityQueue item should have a property of priority when defined

--- src/lib/Queue/Item.spec.ts
+++ src/lib/Queue/Item.spec.ts
@@ -15,4 +15,11 @@ describe('Queue Item', () => {
     expect(item).toHaveProperty('priority');
     expect(item.priority).toBeUndefined();
   });
+
+  it('should have a property of priority when defined', () => {
+    const item: IItem <number> = new Item({ value: 1, priority: 1 });
+
+    expect(item).toHaveProperty('priority');
+    expect(item.priority).toBe(1);
+  });
 });
