5c6a53138e543c424f8abf1ca00378afd1c5adff
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-02 16:51:13 +0300

    BST should add nodes

--- src/lib/Tree/BinarySearchTree.spec.ts
+++ src/lib/Tree/BinarySearchTree.spec.ts
@@ -9,4 +9,25 @@ describe('Binary Search Tree', () => {
     expect(bst.root).toBeNull();
     expect(bst.nodeCount).toBe(0);
   });
+
+  it('should add nodes', () => {
+    const bst: IBinarySearchTree <number> = new BinarySearchTree();
+    const node1: INode <number> = new Node({ value: 4 });
+
+    bst.add(node1);
+    expect(bst.root).toEqual(node1);
+    expect(bst.nodeCount).toBe(1);
+
+    const node2: INode <number> = new Node({ value: 2 });
+    const node3: INode <number> = new Node({ value: 1 });
+    const node4: INode <number> = new Node({ value: 3 });
+    const node5: INode <number> = new Node({ value: 6 });
+
+    bst.add(node2);
+    bst.add(node3);
+    bst.add(node4);
+    bst.add(node5);
+    expect(bst.root!.left!.value).toBe(2);
+    expect(bst.root!.right!.value).toBe(6);
+  });
 });
