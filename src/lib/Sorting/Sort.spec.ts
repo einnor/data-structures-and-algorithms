9dc1be1675a90c0f64da1c94a8ab7c1fc9db592c
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-07 15:10:13 +0300

    Test Sort Class - it should generate random list of size N

--- src/lib/Sorting/Sort.spec.ts
+++ src/lib/Sorting/Sort.spec.ts
@@ -16,4 +16,11 @@ describe('Sort Class', () => {
 
     expect(sort.list).toEqual(list);
   });
+
+  it('should generate random list of size N', () => {
+    const size: number = 10;
+    const sort: ISort = new Sort();
+
+    expect(sort.generateRandomList(size).length).toBe(size);
+  });
 });
