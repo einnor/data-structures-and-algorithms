364482352149abadb5f1a1475e821a087bd85be5
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-08 18:33:48 +0300

    Implemented merge sort algorithm

---
+++ src/lib/Sorting/Algorithms/MergeSort.ts
@@ -0,0 +1,42 @@
+import Sort from '../Sort';
+
+class MergeSort extends Sort {
+  merge (left: number[], right: number[]) {
+    const result = [];
+    let leftLen = left.length;
+    let rightLen = right.length;
+    let leftCursor = 0;
+    let rightCursor = 0;
+    while (leftCursor < leftLen && rightCursor < rightLen) {
+      if (left[leftCursor] < right[rightCursor]) {
+        result.push(left[leftCursor]);
+        leftCursor += 1;
+        this.swaps += 1;
+      } else {
+        result.push(right[rightCursor]);
+        rightCursor += 1;
+      }
+      this.comparisons += 1;
+    }
+    return result.concat(left.slice(leftCursor)).concat(right.slice(rightCursor));
+  }
+
+  sort () : number [] {
+    const list = this.list;
+    if (list.length < 2) {
+      return list;
+    }
+
+    return this.mergeSort(list);
+  };
+
+  mergeSort (list: number[]) : number [] {
+    const length = this.list.length;
+    const mid = Math.floor(length / 2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return this.merge(this.mergeSort(left), this.mergeSort(right));
+  }
+}
+
+export default MergeSort;
