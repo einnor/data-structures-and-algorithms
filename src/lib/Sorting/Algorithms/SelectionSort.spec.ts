771b80519f2169cdff2d074015e587c167452edb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-08 16:02:30 +0300

    Test Selection Sort - it should correctly calculate the number of comparisons

--- src/lib/Sorting/Algorithms/SelectionSort.spec.ts
+++ src/lib/Sorting/Algorithms/SelectionSort.spec.ts
@@ -9,4 +9,14 @@ describe('Selection Sort', () => {
     sort = new SelectionSort([5, 3, 1, 4, 6]);
     expect(sort.sort()).toEqual([1, 3, 4, 5, 6]);
   });
+
+  it('should correctly calculate the number of comparisons', () => {
+    let sort: ISort = new SelectionSort([5, 3, 1, 4, 6]);
+    sort.sort();
+    expect(sort.comparisons).toBe(10);
+
+    sort = new SelectionSort([3, 2, 1]);
+    sort.sort();
+    expect(sort.comparisons).toBe(3);
+  });
 });
