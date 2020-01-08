09abc7b51338255794e4a43b04d74df126052de9
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-08 18:46:40 +0300

    Test Merge Sort - it should correctly calculate the number of comparisons

--- src/lib/Sorting/Algorithms/MergeSort.spec.ts
+++ src/lib/Sorting/Algorithms/MergeSort.spec.ts
@@ -9,4 +9,14 @@ describe('Merge Sort', () => {
     sort = new MergeSort([5, 3, 1, 4, 6]);
     expect(sort.sort()).toEqual([1, 3, 4, 5, 6]);
   });
+
+  it('should correctly calculate the number of comparisons', () => {
+    let sort: ISort = new MergeSort([5, 3, 1, 4, 6]);
+    sort.sort();
+    expect(sort.comparisons).toBe(7);
+
+    sort = new MergeSort([3, 2, 1]);
+    sort.sort();
+    expect(sort.comparisons).toBe(3);
+  });
 });
