f5398ac19a957d8a86e36cadf9e773697d2cf641
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-09 15:25:43 +0300

    Test Quick Sort - it should correctly calculate the number of comparisons

--- src/lib/Sorting/Algorithms/QuickSort.spec.ts
+++ src/lib/Sorting/Algorithms/QuickSort.spec.ts
@@ -9,4 +9,14 @@ describe('Quick Sort', () => {
     sort = new QuickSort([5, 3, 1, 4, 6]);
     expect(sort.sort()).toEqual([1, 3, 4, 5, 6]);
   });
+
+  it('should correctly calculate the number of comparisons', () => {
+    let sort: ISort = new QuickSort([5, 3, 1, 4, 6]);
+    sort.sort();
+    expect(sort.comparisons).toBe(8);
+
+    sort = new QuickSort([3, 2, 1]);
+    sort.sort();
+    expect(sort.comparisons).toBe(3);
+  });
 });
