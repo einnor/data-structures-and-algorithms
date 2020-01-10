0c3115af138faf5eff4a85854fadf7a273dcb3d1
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-10 13:34:35 +0300

    Updated selection sort component to cater for genration os lists

--- src/containers/SelectionSort/index.tsx
+++ src/containers/SelectionSort/index.tsx
@@ -8,11 +8,14 @@ import { ITab } from '../../@types';
 import './style.scss';
 
 const tabs: ITab[] = [
-  { text: 'Sort', value: 'sort' },
+  { text: 'Enter Input', value: 'enter-input' },
+  { text: 'Generate Ordered Input', value: 'generate-ordered-input' },
+  { text: 'Generate Random Input', value: 'generate-random-input' },
 ];
 
 type State = {
   value: string;
+  size: string;
   selectedTab: string;
   results: number [];
   swaps: number;
@@ -21,15 +24,18 @@ type State = {
 
 const SelectionSortImplementation = () => {
 
+  const selectionSort: ISort = new SelectionSort();
+
   const [state, setState] = useState<State>({
     value: '',
-    selectedTab: 'sort',
+    size: '10',
+    selectedTab: 'enter-input',
     results: [],
     swaps: 0,
     comparisons: 0,
   });
 
-  const onSwitch = (value: string) => setState({ ...state, selectedTab: value });
+  const onSwitch = (value: string) => setState({ ...state, selectedTab: value, value: '', size: '10', swaps: 0, comparisons: 0, results: [] });
 
   const sort = (): void => {
     const { value } = state;
@@ -38,15 +44,19 @@ const SelectionSortImplementation = () => {
     }
 
     const input = value.split(',').map((item) => parseInt(item, 10));
-    const sort: ISort = new SelectionSort(input);
-    setState({ ...state, results: sort.sort(), swaps: sort.swaps, comparisons: sort.comparisons });
+    selectionSort.set(input);
+    setState({ ...state, results: selectionSort.sort(), swaps: selectionSort.swaps, comparisons: selectionSort.comparisons });
   }
 
   const switchContent = (): ReactNode => {
     const { selectedTab } = state;
     switch (selectedTab) {
-      case 'sort':
+      case 'enter-input':
         return showForm(sort, 'Sort List');
+      case 'generate-ordered-input':
+        return showForm(sort, 'Sort List', true, true);
+      case 'generate-random-input':
+        return showForm(sort, 'Sort List', true, true);
       default:
         return (
           <>
@@ -62,38 +72,54 @@ const SelectionSortImplementation = () => {
     setState({ ...state, [name]: value });
   };
 
-  const showForm = (fn: () => void, label: string, showValueInput = true): ReactNode => (
-    <div className="form">
-      {
-        showValueInput ? (
-          <TextInput type="text" name="value" label="Enter comma separated values" value={state.value} onChange={(e: ChangeEvent<HTMLInputElement>): void => onChange(e)} />
-        ) : null
-      }
-      <Button onClick={fn} text={label} type="primary" />
-      {
-        state.results ? (
-        <div className="output">{JSON.stringify(state.results)}</div>
-        ) : null
-      }
-      {
-        state.swaps ? (
+  const generateOrderedList = () => {
+    setState({ ...state, value: selectionSort.generateOrderedList(parseInt(state.size, 10)).join(',') });
+  }
+
+  const generateRandomList = () => {
+    setState({ ...state, value: selectionSort.generateRandomList(parseInt(state.size, 10)).join(',') });
+  }
+
+  const showForm = (fn: () => void, label: string, showValueInput: boolean = true, showSizeInput: boolean = false): ReactNode => (
+    <>
+      <div className="form">
+        {
+          showValueInput ? (
+            <TextInput type="text" name="value" label={showSizeInput ? 'Generated Input' : 'Enter comma separated values'} value={state.value} onChange={(e: ChangeEvent<HTMLInputElement>): void => onChange(e)} />
+          ) : null
+        }
+        {
+          showSizeInput ? (
+            <TextInput type="number" name="size" label="Enter size of the list" value={state.size} onChange={(e: ChangeEvent<HTMLInputElement>): void => onChange(e)} />
+          ) : null
+        }
+        {
+          state.selectedTab === 'generate-ordered-input' ? (
+            <Button onClick={generateOrderedList} text="Generate" type="default" />
+            ) : state.selectedTab === 'generate-random-input' ? (
+              <Button onClick={generateRandomList} text="Generate" type="default" />
+            ) : null
+        }
+        <Button onClick={fn} text={label} type="primary" />
+      </div>
+      <div className="output-wrapper">
+        <div className="output">
+          Sorted List:
+          &nbsp;
+          {JSON.stringify(state.results)}
+        </div>
         <div className="output">
           Swaps:
           &nbsp;
           {state.swaps}
         </div>
-        ) : null
-      }
-      {
-        state.comparisons ? (
         <div className="output">
           Comparisons:
           &nbsp;
           {state.comparisons}
         </div>
-        ) : null
-      }
-    </div>
+      </div>
+    </>
   );
 
   return (
