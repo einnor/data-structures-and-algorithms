dd22332b70ec7d556af7fe9df3342edc0d1379d2
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-10 16:24:40 +0300

    Added type for bar item

---
+++ src/@types/IBarItem.ts
@@ -0,0 +1,10 @@
+type IBarItem = {
+  name: string,
+  bubble: number;
+  insertion: number;
+  selection: number;
+  merge: number;
+  quick: number;
+};
+
+export default IBarItem;
--- src/@types/index.ts
+++ src/@types/index.ts
@@ -2,10 +2,12 @@ import IMenuItem from './IMenuItem';
 import IMenuItemChild from './IMenuItemChild';
 import ITab from './ITab';
 import IError from './IError';
+import IBarItem from './IBarItem';
 
 export {
   IMenuItem,
   IMenuItemChild,
   ITab,
   IError,
+  IBarItem,
 };
