c8c8f16f48999921256a13df3b0ebc44982bff5e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-01-10 13:50:08 +0300

    Added skeleton for sorting performance component

---
+++ src/containers/SortingPerformance/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { AppLayout } from '../../components';
+import './style.scss';
+
+const SortingPerformance = () => {
+
+  return (
+    <AppLayout>
+      <>
+        Sorting performance
+      </>
+    </AppLayout>
+  )
+};
+
+export default SortingPerformance;
